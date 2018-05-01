b3c6b5ac9ad00742723025a7a4a807a60f44cf70
Sudhir <sudhiry@thoughtworks.com>
2018-05-01 12:28:10 +0530

    Move viewcontroller to seperate controller

--- backend/src/main/java/com/example/demo/controller/ViewController.java
+++ backend/src/main/java/com/example/demo/controller/ViewController.java
@@ -1,4 +1,4 @@
-package com.example.demo.resources;
+package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ViewController {
 
-//     @RequestMapping({"/frontend","/welcome","/products","/products/{id:\\w+}","/login","/logout"})
      @RequestMapping("/frontend/**")
      public String routeToSPA(){
          return "forward:/index.html";
